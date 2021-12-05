e2c567842fe98f28e21bc1a90ff1ea1a8402673e
LeeJeaWon <73148498+leejeawon92@users.noreply.github.com>
2021-12-05 17:04:43 +0900

    ToDoList를 React로 만들기

--- create-react-app/react-beginners/src/App.js
+++ create-react-app/react-beginners/src/App.js
@@ -1,18 +1,32 @@
-import Button from './Button';
-import PropType from 'prop-types';
-import styles from './App.module.css'
+import { useState } from 'react';
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  }
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    if(toDo ===''){
+      return;
+    } else {
+      setToDo('')
+      setToDos((currentArray)=> [...currentArray, toDo]);
+    }
+  }
+  console.log(toDos);
+  
   return (
-    <div className="App">
-      <h1 className={styles.title}>타이틀!</h1>
-      <Button text={'버튼!'}/>
+    <div>
+      <h1>할일 목록({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type='text' placeholder="할일목록을 적으세요"></input>
+        <button>할일 추가</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => <li key={index}>{item}</li> )}
     </div>
   );
 }
-
-Button.prototype = {
-  text: PropType.string.isRequired,
-}
-
 export default App;
